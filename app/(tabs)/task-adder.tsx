a43a79818be6b83af19fa7c5c4219dc54833cf04
Lucas Lamenha <lamenhajoaolucas@gmail.com>
2025-04-26 14:39:14 -0400

    mostly done with the app's UI

--- app/(tabs)/task-adder.tsx
+++ app/(tabs)/task-adder.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function TaskAdder() {
   return (
+    <LinearGradient
+            colors={['#71c49c', '#F0EAD6']}
+            style={styles.gradientContainer}
+            >
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    style={styles.contentContainer}
     >
-      <Text>Edit app/(tabs)/task-adder.tsx to edit this screen.</Text>
+      <Text style={styles.text}>Edit app/(tabs)/task-adder.tsx to edit this screen.</Text>
     </View>
+    </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  gradientContainer:{
+    flex: 1,
+  },
+  contentContainer:{ 
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+  }
+})
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,28 +1,51 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
-
+import { Link, Stack } from "expo-router";
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function NotFoundPage() {
   return (
+    
+    <LinearGradient
+        colors={['#71c49c', '#F0EAD6']}
+        style={styles.gradientContainer}
+        >
+    <Stack.Screen options={{
+      title: "Oops! Not found",
+      headerTitleStyle: {
+        fontSize: 18,
+        color: "white"
+      },
+      headerTitleAlign: "center",
+      headerStyle: {
+        backgroundColor: '#71c49c',
+        borderBottomWidth: 0,
+      } as any,
+    }} />
     <View style={styles.container}>
       <Text style={styles.text}>Sorry, we couldn't find this page.</Text>
       <Link href={"/"} style={styles.link}>Return to Home</Link>
     </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
+  gradientContainer:{
+    flex: 1,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
   link: {
-    fontSize: 15,
-    textDecorationLine: 'underline'
+    fontSize: 25,
+    textDecorationLine: 'underline',
+    color: "white"
   },
   text: {
     fontSize: 25,
+    color: "white"
   }
 
 });
